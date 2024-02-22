5c4ab688d3ffd29df76591483bbdaa9d96a9aa14
Nelson Ramos <nelsonalejandroramosrivera@gmail.com>
2024-02-22 15:11:13 -0300

    cambio logo blix => RedPay

--- class-wc-gateway-Blix.php
+++ class-wc-gateway-Blix.php
@@ -3,7 +3,7 @@
  * Plugin Name: Blix
  * Plugin URI: http://www.junngla.com
  * Description: Plugin de procesos de pagos para Blix para woocommerce 2.x.
- * Version: 1.0
+ * Version: 1.0.1
  * Author: Junngla
  * Author URI: http://www.junngla.com
  * License: GPL2
